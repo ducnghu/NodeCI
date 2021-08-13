ac769c8a25bb750e4e5bc64f055c7ee2fa29c56a
ducnghu <18120165@student.hcmus.edu.vn>
2021-08-13 21:13:54 +0700

    travis config -fix with new puppeteer

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -4,7 +4,7 @@ let page
 
 beforeEach(async () => {
     page = await Page.build()
-    await page.goto('localhost:3000')
+    await page.goto('http://localhost:3000')
 })
 
 afterEach(async () => {
@@ -14,7 +14,7 @@ afterEach(async () => {
 describe('When logged in', async () => {
     beforeEach(async () => {
         await page.login()
-        await page.goto('localhost:3000/blogs')
+        await page.goto('http://localhost:3000/blogs')
         
         await page.click('a.btn-floating')
     
